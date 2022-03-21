33d13d93110103b64024614e0064be88984724e7
dylan-pereira <dylan.pereira30@orange.fr>
2022-03-21 11:59:41 +0100

    oui

--- www/index.php
+++ www/index.php
@@ -21,25 +21,46 @@
 	  echo "Connection failed: " . $e->getMessage();
 	}	
 
-	$sql = 'SELECT * FROM association';
+	if ($_GET["action"] == 'readAll'){
+		if(isset($_GET['id'])){
+			$sql = 'SELECT * FROM objet WHERE idAssociation='.$_GET['id'];
+			$sth = $pdo->prepare($sql);
 
-	$sth = $pdo->prepare($sql);
+			$sth->execute();
+			$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+			foreach ($result as $key => $array) {
 
-	$sth->execute();
-	$result = $sth->fetchAll(PDO::FETCH_ASSOC);
-	foreach ($result as $key => $array) {
-
-		if(!file_exists("./src/images/logos_associations/".$array['nom'].".PNG") && !file_exists("./src/images/logos_associations/".$array['nom'].".jpg")){
-			echo '<img class="logoAssociation" src="./src/images/default.jpg" alt="logoDefault"> <br/>'.$array['nom'].'<br/>';
+				if(file_exists("./src/images/photos_objets/".$array['nom'].".PNG")){
+					echo '<img class="logoAssociation" src="./src/images/photos_objets/'.$array['nom'].'.PNG" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+				}elseif (file_exists("./src/images/photos_objets/".$array['nom'].".jpg")) {
+					echo '<img class="logoAssociation" src="./src/images/photos_objets/'.$array['nom'].'.jpg" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+				}elseif (file_exists("./src/images/photos_objets/".$array['nom'].".png")) {
+					echo '<img class="logoAssociation" src="./src/images/photos_objets/'.$array['nom'].'.png" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+				}else{
+					echo '<img class="logoAssociation" src="./src/images/default.jpg" alt="logoDefault"> <br/>'.$array['nom'].'<br/>';
+				}
+			}
+			
 		}else{
-			if(file_exists("./src/images/logos_associations/".$array['nom'].".PNG")){
-				echo '<img class="logoAssociation" src="./src/images/logos_associations/'.$array['nom'].'.PNG" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
-			}elseif (file_exists("./src/images/logos_associations/".$array['nom'].".jpg")) {
-				echo '<img class="logoAssociation" src="./src/images/logos_associations/'.$array['nom'].'.jpg" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+			$sql = 'SELECT * FROM association';
+			$sth = $pdo->prepare($sql);
+
+			$sth->execute();
+			$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+			foreach ($result as $key => $array) {
+
+				if(!file_exists("./src/images/logos_associations/".$array['nom'].".PNG") && !file_exists("./src/images/logos_associations/".$array['nom'].".jpg")){
+					echo '<img class="logoAssociation" src="./src/images/default.jpg" alt="logoDefault"> <br/>'.$array['nom'].'<br/>';
+				}else{
+					if(file_exists("./src/images/logos_associations/".$array['nom'].".PNG")){
+						echo '<img class="logoAssociation" src="./src/images/logos_associations/'.$array['nom'].'.PNG" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+					}elseif (file_exists("./src/images/logos_associations/".$array['nom'].".jpg")) {
+						echo '<img class="logoAssociation" src="./src/images/logos_associations/'.$array['nom'].'.jpg" alt="logo' . $array['nom'] . '"> <br/>'.$array['nom'].'<br/>';
+					}
+				}
 			}
 		}
 	}
-
 ?>
 </body>
 </html>
