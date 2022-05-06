f4c152db1cf0a5b1e752db776ad330501b8d968e
samuel13584 <samuelohayon0411@gmail.com>
2022-05-06 09:43:42 +0200

    DIvers

---
+++ www/src/php/details.php
@@ -0,0 +1,20 @@
+<h1 class="hr"><?php echo $result[0]['nomObjet']; ?> </h1>
+<div id="detailsContainer">
+<h1>
+    <?php echo $result[0]['nomObjet']; ?>
+</h1>
+
+<?php
+    if(file_exists("./src/images/photos_objets/".$result[0]['nomObjet'].'_'.$result[0]['idObjet'].".PNG")){
+        echo '<img class="logo" src="./src/images/photos_objets/'.$result[0]['nomObjet'].'_'.$result[0]['idObjet'].'.PNG" alt="logo' . $result[0]['nomObjet'] . '"> <br/>';
+    }elseif (file_exists("./src/images/photos_objets/".$result[0]['nomObjet'].'_'.$result[0]['idObjet'].".png")) {
+        echo '<img class="logo" src="./src/images/photos_objets/'.$result[0]['nomObjet'].'_'.$result[0]['idObjet'].'.png" alt="logo' . $result[0]['nomObjet'] . '"> <br/>';
+    }elseif (file_exists("./src/images/photos_objets/".$result[0]['nomObjet'].'_'.$result[0]['idObjet'].".jpg")) {
+        echo '<img class="logo" src="./src/images/photos_objets/'.$result[0]['nomObjet'].'_'.$result[0]['idObjet'].'.jpg" alt="logo' . $result[0]['nomObjet'] . '"> <br/>';
+    }elseif (file_exists("./src/images/photos_objets/".$result[0]['nomObjet'].'_'.$result[0]['idObjet'].".JPG")) {
+        echo '<img class="logo" src="./src/images/photos_objets/'.$result[0]['nomObjet'].'_'.$result[0]['idObjet'].'.JPG" alt="logo' . $result[0]['nomObjet'] . '"> <br/>';
+    }else{
+        echo '<img class="logo" src="./src/images/default.jpg" alt="logoDefault"> <br/>';
+    }
+?>
+</div>
